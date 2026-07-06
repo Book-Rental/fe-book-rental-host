eec135746b27f2776bce9f674c4cf639e3f765ca
Nishant8209 <nishant.prabhu@techdenali.com>
2026-07-06 18:39:49 +0530

    resolved the lint issue

--- src/Tests/app.test.tsx
+++ src/Tests/app.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 
 import App from '../App';         // Update this to point to your real App file
